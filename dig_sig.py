d97e4506e260cbe17cf3c852402a5969c8b8e5d1
achonikolov592 <achonikolov2005@gmail.com>
2025-10-07 14:45:54 +0200

    update signature

--- dig_sig.py
+++ dig_sig.py
@@ -1,10 +1,10 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
-'''
+
 private_key = ec.generate_private_key(ec.SECP256R1())
 public_key = private_key.public_key()
-
+'''
 with open("private_key.pem", "wb") as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
@@ -16,6 +16,7 @@ with open("public_key.pem", "wb") as f:
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
+
 '''
 '''
 with open("private_key.pem", "rb") as f:
@@ -34,8 +35,6 @@ signature = private_key.sign(
 
 with open("signature.sig", "wb") as f:
     f.write(signature)
-
-
 '''
 file_path="./test/sudoku_solver.py"
 with open(file_path, "rb") as f:
@@ -45,7 +44,7 @@ with open("public_key.pem", "rb") as f:
     pem_data = f.read()
 public_key = serialization.load_pem_public_key(pem_data)
 
-with open("signature.sig", "rb") as f:
+with open("./test/signature.sig", "rb") as f:
     signature = f.read()
 
 try:
