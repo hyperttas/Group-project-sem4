94c186a6b67843ea57daef063b8628869982c1fa
achonikolov592 <achonikolov2005@gmail.com>
2025-10-07 15:03:48 +0200

    update signature1.2

--- dig_sig.py
+++ dig_sig.py
@@ -1,10 +1,10 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
-
+'''
 private_key = ec.generate_private_key(ec.SECP256R1())
 public_key = private_key.public_key()
-'''
+
 with open("private_key.pem", "wb") as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
